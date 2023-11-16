fb04cf8265014e1065936a40d6b9d2af38710bc0
Jordan RA <jordan.rojas@devsu.com>
2023-11-16 17:06:22 -0500

    add test date and transaction

---
+++ common/android-test/src/main/java/com/ivy/common/androidtest/Transaction.kt
@@ -0,0 +1,46 @@
+package com.ivy.common.androidtest
+
+import com.ivy.common.time.timeNow
+import com.ivy.core.persistence.dao.trn.SaveTrnData
+import com.ivy.core.persistence.entity.trn.TransactionEntity
+import com.ivy.core.persistence.entity.trn.data.TrnTimeType
+import com.ivy.data.SyncState
+import com.ivy.data.transaction.TransactionType
+import com.ivy.data.transaction.TrnPurpose
+import com.ivy.data.transaction.TrnState
+import java.time.Instant
+import java.util.UUID
+
+fun transactionEntity(): TransactionEntity{
+    return TransactionEntity(
+        id = UUID.randomUUID().toString(),
+        accountId = "test-account",
+        type = TransactionType.Expense,
+        amount = 50.0,
+        currency = "EUR",
+        time = Instant.now(),
+        timeType = TrnTimeType.Actual,
+        title = "Test transaction",
+        description = "Test description",
+        categoryId = null,
+        state = TrnState.Default,
+        purpose = TrnPurpose.Fee,
+        sync = SyncState.Syncing,
+        lastUpdated = Instant.now()
+    )
+}
+
+fun transactionWithTime(time: Instant, transaction: TransactionEntity = transactionEntity()): TransactionEntity{
+    return transaction.copy(
+        time = time
+    )
+}
+
+fun saveTrnData(entity: TransactionEntity = transactionEntity()): SaveTrnData{
+    return SaveTrnData(
+        entity = entity,
+        tags = emptyList(),
+        attachments = emptyList(),
+        metadata = emptyList()
+    )
+}
